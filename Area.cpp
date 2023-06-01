60451de5cc5677fd0ffc6fad7b0a3c1516c37dd4
GATETECAT4 <95552148+GATETECAT4@users.noreply.github.com>
2023-06-01 09:45:45 -0700

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+float area(float _b, float _h) {
+    return _b * _h;
+}
+
+int main()
+{
+    // Calcula el puto area de un rectangulo
+    float b;
+    float h;
+
+    std::cout << "Digite el valor de la base: "; std::cin >> b;
+
+    std::cout << "Digite el valor de la altura: "; std::cin >> h;
+
+    float A = area(b,h);
+
+    std::cout << "El area del rectangulo es: " << A << std::endl;
+    return 0;
+
+}
