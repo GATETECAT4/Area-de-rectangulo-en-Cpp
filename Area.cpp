b2c16d16ddd58e202474358312520279a2e45733
GATETECAT4 <95552148+GATETECAT4@users.noreply.github.com>
2023-06-21 14:28:15 -0700

    Update Area.cpp

--- Area.cpp
+++ Area.cpp
@@ -9,14 +9,13 @@ int main()
 {
     // Calcula el area de un rectangulo
     
-    float b;
-    float h;
+    float b, h, A;
 
     std::cout << "Digite el valor de la base: "; std::cin >> b;
 
     std::cout << "Digite el valor de la altura: "; std::cin >> h;
 
-    float A = area(b,h);
+    A = area(b,h);
 
     std::cout << "El area del rectangulo es: " << A << "cm" << std::endl;
     return 0;
